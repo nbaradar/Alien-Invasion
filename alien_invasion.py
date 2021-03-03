f440a354b5389a1529988049dc5c3e75a58ce873
nbaradar <naderb1992@gmail.com>
2021-03-02 21:08:03 -0500

    test

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+"""This is an example module.
+
+This module does stuff.
+"""
+
+import sys
+import pygame
+import random
+
+class AlienInvasion:
+    """Overrall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.pygameSurface = pygame.display.set_mode((1200, 800))
+
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    self.pygameSurface.fill((random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)))
+
+            #Make the most recently drawn screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
