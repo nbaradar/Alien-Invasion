1f3c37f60eb12b40334b753e08c87d7889a0385b
nbaradar <naderb1992@gmail.com>
2021-03-02 23:15:41 -0500

    page 236

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ import random
 
 #This imports the Settings class from the settings module (which is just the settings.py file)
 from settingsModule import Settings
+from shipModule import Ship
 
 class AlienInvasion:
     """Overrall class to manage game assets and behavior"""
@@ -18,10 +19,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        pygame.display.set_caption("Alien Invasion")
+        
         self.settings = Settings()
-
         self.pygameSurface = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.bg_color = (self.settings.bg_color)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -30,10 +33,12 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                """if event.type == pygame.KEYDOWN:
-                    self.pygameSurface.fill((random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)))"""
-            self.pygameSurface.fill(self.bg_color)
-
+                if event.type == pygame.KEYDOWN:
+                    self.pygameSurface.fill(self.bg_color)
+                    """self.pygameSurface.fill((random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)))"""
+                    
+            self.ship.blitme()
+                    
             #Make the most recently drawn screen visible
             pygame.display.flip()
 
---
+++ shipModule.py
@@ -0,0 +1,22 @@
+"""Module for player ship"""
+
+import pygame
+
+class Ship: 
+    """A class to manage the ship."""    
+    #The contructor takes the game engine so it has access to all its resources
+    def __init__(self, alienInvasion_engine):
+        """Initialize the ship and set its starting position."""
+        self.screen = alienInvasion_engine.pygameSurface
+        self.screen_rect = alienInvasion_engine.pygameSurface.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('graphics\PikPng.com_space-invaders-png_2205494.png')
+        self.rect = self.image.get_rect()
+
+        #Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+    
+    def blitme(self):
+        """Draw the ship at its current position."""
+        self.screen.blit(self.image, self.rect)
