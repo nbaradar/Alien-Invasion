4a1246e390dd118477f760fce9736fdd709a3813
nbaradar <naderb1992@gmail.com>
2021-03-07 22:44:18 -0500

    bullet collision added

--- alien_module.py
+++ alien_module.py
@@ -9,6 +9,7 @@ class Alien(Sprite):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = alien_invasion_engine.screen
+        self.settings = alien_invasion_engine.settings
 
         #Load the alien image and set its rect attribute
         self.image = pygame.image.load('graphics/alien.bmp')
@@ -20,3 +21,14 @@ class Alien(Sprite):
 
         #Store the aliens exact horizontal position
         self.x_coord = float(self.rect.x)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien right or left"""
+        self.x_coord += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x_coord
